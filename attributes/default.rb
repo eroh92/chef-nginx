a695bc41f0546928b89bb45f5a193e9b91eaf82a
Rob Eroh <rob@eroh.me>
2014-01-21 10:40:17 +1100

    server tokens off

--- attributes/default.rb
+++ attributes/default.rb
@@ -87,7 +87,7 @@ default['nginx']['worker_connections']   = 1_024
 default['nginx']['worker_rlimit_nofile'] = nil
 default['nginx']['multi_accept']         = false
 default['nginx']['event']                = nil
-default['nginx']['server_tokens']        = nil
+default['nginx']['server_tokens']        = 'off'
 default['nginx']['server_names_hash_bucket_size'] = 64
 default['nginx']['sendfile'] = 'on'
 
